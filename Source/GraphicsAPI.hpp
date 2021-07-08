660b30be9540a17cddf1cf6dbc33fbf4fcd6c83a
Tristan <code@thewoosh.org>
2021-07-08 19:44:12 +0200

    [GraphicsAPI] Add ::Name enum for passing the type of GraphicsAPI implementation

--- Source/GraphicsAPI.hpp
+++ Source/GraphicsAPI.hpp
@@ -2,6 +2,12 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Vulkan and the Vulkan logo are registered trademarks of the Khronos Group
+ * Inc.
+ *
+ * OpenGL® and the oval logo are trademarks or registered trademarks of Hewlett
+ * Packard Enterprise in the United States and/or other countries worldwide.
  */
 
 #pragma once
@@ -10,8 +16,13 @@ class WindowAPI;
 
 class GraphicsAPI {
 public:
-	[[nodiscard]] virtual bool
-	initialize(WindowAPI *) = 0;
+    enum class Name {
+        OPENGL,
+        VULKAN,
+    };
 
-	virtual ~GraphicsAPI() noexcept = default;
+    [[nodiscard]] virtual bool
+    initialize(WindowAPI *) = 0;
+
+    virtual ~GraphicsAPI() noexcept = default;
 };
