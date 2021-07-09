79ce3751e33c6a0eea76d29f2c44a97128576519
Tristan <code@thewoosh.org>
2021-07-09 17:13:49 +0200

    [Lavender] Use OpenGL for development

--- Source/Lavender.cpp
+++ Source/Lavender.cpp
@@ -12,24 +12,38 @@
 
 base::ExitStatus
 Lavender::run() {
-	m_windowAPI = std::make_unique<window::GLFWCore>();
-	if (!m_windowAPI->initialize(GraphicsAPI::Name::OPENGL)) {
-		return base::ExitStatus::FAILED_INITIALISING_WINDOW_API;
-	}
+    m_windowAPI = std::make_unique<window::GLFWCore>();
+    if (!m_windowAPI->initialize(GraphicsAPI::Name::OPENGL)) {
+        return base::ExitStatus::FAILED_INITIALISING_WINDOW_API;
+    }
+
+    auto glVersion = m_windowAPI->queryGLContextVersion();
+    std::printf("[Lavender] Context Version: %i.%i.%i\n", glVersion.major, glVersion.minor, glVersion.revision);
 
 //	m_graphicsAPI = std::make_unique<vke::Core>();
-        m_graphicsAPI = std::make_unique<gle::Core>();
-	if (!m_graphicsAPI->initialize(m_windowAPI.get())) {
-		return base::ExitStatus::FAILED_INITIALISING_GRAPHICS_API;
-	}
+    m_graphicsAPI = std::make_unique<gle::Core>();
+    if (!m_graphicsAPI->initialize(m_windowAPI.get())) {
+        return base::ExitStatus::FAILED_INITIALISING_GRAPHICS_API;
+    }
 
-	while (!m_windowAPI->shouldClose()) {
+    static_cast<void>(
+        m_graphicsAPI->createModel(resources::ModelGeometry{
+            .vertices {
+                 0.0f,  0.5f,
+                 0.5f, -0.5f,
+                -0.5f, -0.5f
+            },
+            .indices {}
+        })
+    );
+
+    while (!m_windowAPI->shouldClose()) {
         m_windowAPI->preLoop();
 
-		// do stuff
+        m_graphicsAPI->renderEntities();
 
-		m_windowAPI->postLoop();
-	}
+        m_windowAPI->postLoop();
+    }
 
-	return base::ExitStatus::SUCCESS;
+    return base::ExitStatus::SUCCESS;
 }
