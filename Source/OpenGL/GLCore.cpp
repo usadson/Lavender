3a39f3814332b6bdf35ae6f4b6226b651c0677e9
Tristan <code@thewoosh.org>
2021-07-17 09:11:22 +0200

    [OpenGL] Bind framebuffer before clearing color

--- Source/OpenGL/GLCore.cpp
+++ Source/OpenGL/GLCore.cpp
@@ -318,9 +318,9 @@ namespace gle {
 
     void
     Core::renderEntities() noexcept {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         glBindFramebuffer(GL_FRAMEBUFFER, m_gBuffer.buffer());
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(m_shaderProgram->programID());
 
